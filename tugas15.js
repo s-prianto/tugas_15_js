2ada2954d565f59767b1ecee635ce05c47f96803
s-prianto <prianto2796@gmail.com>
2020-02-12 06:35:42 +0700

    Tugas 15 JS

---
+++ tugas15.js
@@ -0,0 +1,8 @@
+var t = 6;
+var l = 8;
+var sm = Math.sqrt(Math.pow(t,2)+Math.pow(l,2));
+
+
+console.log("Nilai AB = ",t, "cm");
+console.log("Nilai BC = ",l, "cm");
+console.log("Panjang sis AC pada segitiga siku - siku tersebut adalah : ",sm, "cm");
